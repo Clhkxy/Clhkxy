0f683a4bf72eb0a49c4d6ec20fbf491b9509e775
Clhkxy <125267791+Clhkxy@users.noreply.github.com>
2023-04-28 12:08:14 +0000

    Review

--- Operation.java
+++
@@ -1,7 +0,0 @@
-public class Operation{
-    public static void main(String[] args) {
-        int a=1;
-        a += 7*2;
-        System.out.println(a);
-    }
-}
--- wei.java
+++
@@ -1,6 +0,0 @@
-public class wei{
-    public static void main(String[] args) {
-        int a1 = 2^3;
-        System.out.println(a1);
-    }
-}
